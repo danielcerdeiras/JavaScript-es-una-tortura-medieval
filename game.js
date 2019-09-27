2da48bb61eda81625bcadb39c844d44e29a576d3
Daniel González Cerdeiras <daniel20@ucm.es>
2019-09-27 18:11:43 +0200

    Añadidio GDD incompleto, creada clase Enemigo

--- game.js
+++ game.js
@@ -11,3 +11,21 @@ export default class Game extends Phaser.Scene {
   update(time, delta) {    
   }
 }
+
+class Enemigo
+{
+  //dir: 8 direcciones. 5 = todas direcciones; 1 - 9 direcciones singulares; n > 9 combinación de cada dígito
+  constructor(pos, tipo, dir)
+  {
+    this.tipo = tipo;
+    this.dir = dir;
+    this.mov = 0;
+  }
+
+  constructor(pos, tipo, mov, dir)
+  {
+    this.tipo = tipo;
+    this.mov = mov;
+    this.dir = dir;
+  }
+}
