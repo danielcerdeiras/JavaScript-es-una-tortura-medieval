923c90c46e7ea571046daa907c2a1b0f834bab59
georgimednikov <48915821+georgimednikov@users.noreply.github.com>
2019-10-18 18:12:46 +0200

    Update player.js

--- player.js
+++ player.js
@@ -1,21 +1,22 @@
-export default class player extends Phaser.GameObjects.Sprite{
+export default class Player extends Phaser.GameObjects.Sprite{
     constructor(scene, x, y, width, height) {
         super(scene, x, y, 'player');
         this.scene.add.existing(this);
         this.body.SetColliderWorldBounds();
         this.displayWidth = width;
         this.displayHeight = height;
-        this.cursors = scene.cursors.input.keyboard.createCursorKeys();
+        this.SetOrigin(0, 0);
+        this.cursors = scene.cursors.input.keyboard.addKeys('W,A,S,D');
     }
 
     move() {
-        if (this.cursors.up.isDown)
+        if (this.cursors.W.isDown)
             this.y -= 10;
-        if (this.cursors.down.isDown)
+        if (this.cursors.S.isDown)
             this.y += 10;
-        if (this.cursors.left.isDown)
+        if (this.cursors.A.isDown)
             this.x -= 10;
-        if (this.cursors.rigth.isDown)
+        if (this.cursors.D.isDown)
             this.x += 10;
     }
 }
